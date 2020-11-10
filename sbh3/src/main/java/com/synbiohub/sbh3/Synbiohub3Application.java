94fa56f90132fd29b0df0f240ca4776ddac1926e
Eric <ejyu99@gmail.com>
2020-11-10 07:57:36 -0700

    Add Spring Boot project

---
+++ sbh3/src/main/java/com/synbiohub/sbh3/Synbiohub3Application.java
@@ -0,0 +1,13 @@
+package com.synbiohub.sbh3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Synbiohub3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Synbiohub3Application.class, args);
+	}
+
+}
---
+++ sbh3/src/test/java/com/synbiohub/sbh3/Synbiohub3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synbiohub.sbh3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Synbiohub3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
