9166a08bdbcbe10923506fc825787aba0ebe4009
Eric <ejyu99@gmail.com>
2020-11-28 17:18:15 -0700

    Add SPARQL endpoint
    Able to send GET request and gets back the output of the SPARQL query to Virtuoso in JSON format

--- sbh3/src/main/java/com/synbiohub/sbh3/search/SearchController.java
+++ sbh3/src/main/java/com/synbiohub/sbh3/search/SearchController.java
@@ -1,14 +1,46 @@
 package com.synbiohub.sbh3.search;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/search")
 public class SearchController {
-
-    @GetMapping(value = "/{keyword}")
+    /*
+    @GetMapping(value = "/search/{keyword}")
     @ResponseBody
-    public String getResults(@PathVariable("keyword") String keyword) {
-        return "Search keyword: " + keyword;
+    public String getResults(@RequestParam(required = false) Map<String,String> allParams, @PathVariable("keyword") String keyword) {
+        System.out.println(allParams.entrySet());
+        return keyword;
     }
+    */
+    /**
+     * Queries Virtuoso
+     * @param query SPARQL Query
+     * @return Results from Virtuoso
+     */
+    @RequestMapping(value = "/sparql", headers = "Accept=application/json")
+    @ResponseBody
+    public String getSPARQL(@RequestParam String query) throws UnsupportedEncodingException {
+        RestTemplate restTemplate = new RestTemplate();
+        // Passing in query with brackets - need to do this or Spring Boot will complain about chars in the query
+        String url = "http://localhost:8890/sparql?default-graph-uri=&query={query}&format=json&";
+        return restTemplate.getForObject(url, String.class, query);
+    }
+
+    /*
+    @RequestMapping("/rootCollections")
+    @ResponseBody
+    public String getRootCollections() {
+
+    }
+     */
+
 }
