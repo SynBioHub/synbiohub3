d340abdf51149f9f2c4af3b84ff79d4e083645e5
Eric <ejyu99@gmail.com>
2020-12-08 00:12:27 -0700

    Create Search Metadata endpoint

--- backend/src/main/java/com/synbiohub/sbh3/search/SearchController.java
+++ backend/src/main/java/com/synbiohub/sbh3/search/SearchController.java
@@ -3,6 +3,8 @@ package com.synbiohub.sbh3.search;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,14 +15,74 @@ import java.util.Map;
 
 @RestController
 public class SearchController {
-    /*
-    @GetMapping(value = "/search/{keyword}")
+
+    /**
+     * Search Metadata endpoint
+     * @param allParams Key/value pairs of all parameters
+     * @return Metadata for the object from the specified search query in JSON format
+     */
+    @GetMapping(value = "/search")
     @ResponseBody
-    public String getResults(@RequestParam(required = false) Map<String,String> allParams, @PathVariable("keyword") String keyword) {
+    public String getResults(@RequestParam Map<String,String> allParams) {
         System.out.println(allParams.entrySet());
-        return keyword;
+        String criteriaString = "";
+
+        // Process search parameters
+        for (Map.Entry<String, String> param : allParams.entrySet()) {
+            // A tag in the dcterms namespace to search for
+            if (param.getKey().contains(":")) {
+                criteriaString += "   ?subject " + param.getKey() + " " + param.getValue() + " . ";
+            }
+            // Type of object to search for
+            else if (param.getKey().equals("objectType")) {
+                if (param.getValue().contains(":")) {
+                    criteriaString += "   ?subject a " +param.getValue() + " . ";
+                } else {
+                    criteriaString += "   ?subject a sbol2:" + param.getValue() + " . ";
+                }
+            }
+
+            else if (param.getKey().equals("collection")) {
+                criteriaString += "   ?collection a sbol2:Collection .   " + param.getValue() + " sbol2:member ?subject .";
+            }
+
+            else if (param.getKey().equals("createdBefore")) {
+                criteriaString += "   FILTER (xsd:dateTime(?cdate) <= \"" + param.getValue() + "T23:59:59Z\"^^xsd:dateTime) ";
+            }
+
+            else if (param.getKey().equals("createdAfter")) {
+                criteriaString += "   FILTER (xsd:dateTime(?cdate) >= \"" + param.getValue() + "T00:00:00Z\"^^xsd:dateTime) ";
+            }
+
+            else if (param.getKey().equals("modifiedBefore")) {
+                criteriaString += "   FILTER (xsd:dateTime(?mdate) <= \"" + param.getValue() + "T23:59:59Z\"^^xsd:dateTime) ";
+            }
+
+            else if (param.getKey().equals("modifiedAfter")) {
+                criteriaString += "   FILTER (xsd:dateTime(?mdate) >= \"" + param.getValue() + "T00:00:00Z\"^^xsd:dateTime) ";
+            }
+
+            else {
+                criteriaString += "   ?subject sbol2:" + param.getKey() + " " + param.getValue() + " . ";
+            }
+        }
+        return criteriaString;
     }
-    */
+
+
+    /**
+     * Redirects from the old search API to a standardized URI
+     * <p> Use {@link SearchController#getResults(Map)} instead.
+     * @deprecated
+     * @param keyword
+     * @return Redirect to search controller
+     */
+    @GetMapping("/search/{keyword}")
+    public RedirectView redirectOldSearch(@PathVariable("keyword") String keyword) {
+        String baseUri = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        return new RedirectView(baseUri + "/search?" + keyword);
+    }
+
     /**
      * Queries Virtuoso
      * @param query SPARQL Query
@@ -34,13 +96,4 @@ public class SearchController {
         String url = "http://localhost:8890/sparql?default-graph-uri=&query={query}&format=json&";
         return restTemplate.getForObject(url, String.class, query);
     }
-
-    /*
-    @RequestMapping("/rootCollections")
-    @ResponseBody
-    public String getRootCollections() {
-
-    }
-     */
-
 }
