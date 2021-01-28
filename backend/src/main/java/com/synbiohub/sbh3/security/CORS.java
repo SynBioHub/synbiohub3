dabd7a4f9d1ac2f5b91c83050c08ad83f89506ad
Eric <ejyu99@gmail.com>
2021-01-28 16:21:14 -0700

    Disable CORS
    TEMPORARY MEASURE

---
+++ backend/src/main/java/com/synbiohub/sbh3/security/CORS.java
@@ -0,0 +1,22 @@
+package com.synbiohub.sbh3.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CORS {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
