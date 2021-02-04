f726c0f2422c41d2df3531d813e52bdb45d04ca3
Eric <ejyu99@gmail.com>
2021-02-04 00:27:14 -0700

    Update UserController.java

--- backend/src/main/java/com/synbiohub/sbh3/users/UserController.java
+++ backend/src/main/java/com/synbiohub/sbh3/users/UserController.java
@@ -1,2 +1,12 @@
 package com.synbiohub.sbh3.users;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+/**
+    @PostMapping(value = "/register")
+    public
+**/
+}
