903e5adbdefcfb295132c94f9fba3841adfaa618
benjhatch <benjamin.hat4@gmail.com>
2021-05-12 14:55:56 -0600

    updated search functionality to use the useSwr hook for both search results and search count in orderto sync the two when viewing a previously queried search

--- frontend/components/SearchPanelComponents/StandardSearch.js
+++ frontend/components/SearchPanelComponents/StandardSearch.js
@@ -2,44 +2,69 @@ import { standardcontainer, standardresultsloading, standarderror, countloader, 
 
 import useSWR from 'swr';
 import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 import Loader from 'react-loader-spinner';
 import ResultTable from './ResultTable/ResultTable';
 import { useSelector } from 'react-redux';
 
-export default function StandardSearch(props) {
+export default function StandardSearch() {
    const query = useSelector(state => state.search.query);
+   const hasQueryChanged = useCompare(query);
    const [offset, setOffset] = useState(0);
    const [count, setCount] = useState(undefined);
+   const { newCount, isCountLoading, isCountError } = getSearchCount(query, offset);
    useEffect(() => {
-      setOffset(0);
-      setCount(
-         <div className={countloadercontainer}>
-            <Loader type="ThreeDots" color="#D25627" width={25} height={10} className={countloader} />
-         </div>);
-      const params = { headers: { "content-type": "text/plain; charset=UTF-8" } };
-      fetch(`${process.env.backendUrl}/searchCount/${query}`, params)
-         .then(res => res.json()).then(data => setCount(data));
-
-   }, [query]);
-   const { data, error } = useSWR(`${process.env.backendUrl}/search/${query}?offset=${offset}`, fetcher);
-   if (error) return <div className={standarderror}>Errors were encountered while fetching the data</div>
-   if (!data) return (
-      <div className={standardcontainer}>
-         <div className={standardresultsloading}>
-            <Loader type="Grid" color="#D25627" />
-         </div>
-      </div>
-   );
-
-   if (data.length == 0) return <div className={standarderror}>No results found</div>
-   else {
+      if (hasQueryChanged)
+         setOffset(0);
+      if (isCountLoading)
+         setCount(
+            <div className={countloadercontainer}>
+               <Loader type="ThreeDots" color="#D25627" width={25} height={10} className={countloader} />
+            </div>
+         )
+      if (isCountError)
+         setCount('Error')
+      else
+         setCount(newCount)
+   }, [query, hasQueryChanged, newCount, isCountLoading, isCountError]);
+   
+   const { results, isLoading, isError } = getSearchResults(query, offset);
+   if (isError)
+      return <div className={standarderror}>Errors were encountered while fetching the data count</div>
+   if (isLoading)
       return (
          <div className={standardcontainer}>
-            <ResultTable data={data} basketItems={props.basketItems} setBasketItems={props.setBasketItems} count={count} offset={offset} setOffset={setOffset} />
+            <div className={standardresultsloading}>
+               <Loader type="Grid" color="#D25627" />
+            </div>
          </div>
-      );
+      )
+   if (results.length === 0)
+      return <div className={standarderror}>No results found</div>
+   else
+      return (
+         <div className={standardcontainer}>
+            <ResultTable data={results} count={count} offset={offset} setOffset={setOffset} />
+         </div>
+      )
+}
+
+const getSearchResults = (query, offset) => {
+   const { data, error } = useSWR(`${process.env.backendUrl}/search/${query}?offset=${offset}`, fetcher);
+   return {
+      results: data,
+      isLoading: !error && !data,
+      isError: error
+   }
+}
+
+const getSearchCount = (query, offset) => {
+   const { data, error } = useSWR(`${process.env.backendUrl}/searchCount/${query}?offset=${offset}`, fetcher);
+   return {
+      newCount: data,
+      isCountLoading: !error && !data,
+      isCountError: error
    }
 }
 
@@ -49,3 +74,17 @@ const fetcher = url => axios.get(url, {
       "Accept": "text/plain"
    }
 }).then(res => res.data);
+
+function useCompare (val) {
+   const prevVal = usePrevious(val)
+   return prevVal !== val
+ }
+ 
+ // Helper hook
+ function usePrevious(value) {
+   const ref = useRef();
+   useEffect(() => {
+     ref.current = value;
+   });
+   return ref.current;
+ }
