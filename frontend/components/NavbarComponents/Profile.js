564518b5f6b333c24c7f728700cb2927f4d73e2c
benjhatch <benjamin.hat4@gmail.com>
2021-06-09 21:09:35 -0700

    styles user profile icon

--- frontend/components/NavbarComponents/Profile.js
+++ frontend/components/NavbarComponents/Profile.js
@@ -8,14 +8,15 @@ export default function Profile() {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <div className={styles.profilecontainer}>
-      <Image
-        alt="Profile"
-        className={styles.borderCircle}
-        src="/images/user.svg"
-        width={30}
-        height={30}
-        onClick={() => setShowMenu(!showMenu)}
-      />
+      <div className={styles.iconcontainer}>
+        <Image
+          alt="Profile"
+          src="/images/user.svg"
+          width={28}
+          height={28}
+          onClick={() => setShowMenu(!showMenu)}
+        />
+      </div>
       {showMenu && <ProfileMenu />}
     </div>
   );
