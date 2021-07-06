578feebc77215becdcefc99ce562a8bc088e70b4
benjhatch <benjamin.hat4@gmail.com>
2021-07-06 14:47:16 -0600

    made empty submissions page

---
+++ frontend/pages/submissions.js
@@ -0,0 +1,10 @@
+import TopLevel from '../components/TopLevel';
+import styles from '../styles/submissions.module.css';
+
+export default function Submissions() {
+  return (
+    <TopLevel>
+      <div className={styles.container}>Submissions</div>
+    </TopLevel>
+  );
+}
