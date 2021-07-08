b61ae42f3a85e5b1f88541e9e64258f22418ad4c
benjhatch <benjamin.hat4@gmail.com>
2021-07-08 13:28:40 -0600

    importing font awesome css for ssr (no size jump in production)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
 import '../styles/globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
+import { config } from '@fortawesome/fontawesome-svg-core';
 import { Provider } from 'react-redux';
 
 import { useStore } from '../redux/store';
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 /**
  * This component is the starting component for the sbh app. Uses Provider
