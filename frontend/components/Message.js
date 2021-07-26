b0c9a729a34fcc1c665cea1a9f98a7aa26e40c91
benjhatch <benjamin.hat4@gmail.com>
2021-07-26 15:14:24 -0700

    added checkbox for not showing a specific message again, works correctly

--- frontend/components/Message.js
+++ frontend/components/Message.js
@@ -1,9 +1,12 @@
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
 import styles from '../styles/message.module.css';
 
 export default function Message(properties) {
+  const [promptUser, setPromptUser] = useState(true);
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -15,20 +18,31 @@ export default function Message(properties) {
               color="#D25627"
             />
           </div>
-          <div className={styles.message}>{properties.message}</div>
+          <div className={styles.message}>
+            {properties.message}
+            <div className={styles.showmessageinputcontainer}>
+              <input
+                type="checkbox"
+                className={styles.showmessageinput}
+                onChange={event => setPromptUser(!event.target.checked)}
+                checked={!promptUser}
+              />
+              <span>Do not show this message again</span>
+            </div>
+          </div>
         </div>
         <div className={styles.buttoncontainer}>
           <div
             className={`${styles.button} ${styles.cancel}`}
             role="button"
-            onClick={() => properties.close()}
+            onClick={() => properties.close(promptUser)}
           >
             Cancel
           </div>
           <div
             className={`${styles.button} ${styles.actionbutton}`}
             role="button"
-            onClick={() => properties.action()}
+            onClick={() => properties.action(promptUser)}
           >
             {properties.buttontext}
           </div>
--- frontend/components/SubmitComponents/OverwriteObjects.js
+++ frontend/components/SubmitComponents/OverwriteObjects.js
@@ -1,12 +1,34 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../../styles/submit.module.css';
 import Message from '../Message';
 
 export default function OverwriteObjects(properties) {
   const [warnUser, setWarnUser] = useState(false);
+  const [enablePrompt, setEnablePrompt] = useState(
+    localStorage.getItem('PromptOverwrite') === 'true'
+  );
+  useEffect(() => {
+    localStorage.setItem('PromptOverwrite', enablePrompt);
+  }, [enablePrompt]);
+
+  useEffect(() => {
+    const promptUser = localStorage.getItem('PromptOverwrite');
+    switch (promptUser) {
+      case 'true':
+        setEnablePrompt(true);
+        break;
+      case 'false':
+        setEnablePrompt(false);
+        break;
+      default:
+        setEnablePrompt(true);
+        break;
+    }
+  });
+
   return (
     <div className={styles.overwritecontainer}>
       <div className={styles.overwriteinputcontainer}>
@@ -14,8 +36,8 @@ export default function OverwriteObjects(properties) {
           type="checkbox"
           checked={properties.checked}
           onChange={event => {
-            if (event.target.checked) setWarnUser(true);
-            else properties.setChecked(false);
+            if (event.target.checked && enablePrompt) setWarnUser(true);
+            else properties.setChecked(event.target.checked);
           }}
         />
         <div className={styles.overwritemessage}>
@@ -36,20 +58,43 @@ export default function OverwriteObjects(properties) {
           />
         </a>
       </div>
-      {warnUser ? (
+      {warnUser && enablePrompt ? (
         <Message
           message="Are you sure you want to overwrite existing objects in the collection? All objects currently existing in the collection will be lost."
           buttontext="Confirm"
-          close={() => {
-            properties.setChecked(false);
-            setWarnUser(false);
-          }}
-          action={() => {
-            properties.setChecked(true);
-            setWarnUser(false);
-          }}
+          setPromptUser={newEnablePrompt => setEnablePrompt(newEnablePrompt)}
+          close={(newEnablePrompt = true) =>
+            handlePromptOverwrite(
+              false,
+              properties.setChecked,
+              setWarnUser,
+              setEnablePrompt,
+              newEnablePrompt
+            )
+          }
+          action={(newEnablePrompt = true) =>
+            handlePromptOverwrite(
+              true,
+              properties.setChecked,
+              setWarnUser,
+              setEnablePrompt,
+              newEnablePrompt
+            )
+          }
         />
       ) : null}
     </div>
   );
 }
+
+const handlePromptOverwrite = (
+  overwrite,
+  setChecked,
+  setWarnUser,
+  setEnablePrompt,
+  newEnablePrompt = true
+) => {
+  setChecked(overwrite);
+  setWarnUser(false);
+  setEnablePrompt(newEnablePrompt);
+};
