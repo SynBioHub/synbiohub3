a035709ad0a560fa5772d630a53ff48b9d0ad612
benjhatch <benjamin.hat4@gmail.com>
2021-08-10 17:13:40 -0700

    added icons to admin menu selectors

--- frontend/components/AdminComponents/Menu.js
+++ frontend/components/AdminComponents/Menu.js
@@ -1,3 +1,17 @@
+import { faPagelines } from '@fortawesome/free-brands-svg-icons';
+import {
+  faDatabase,
+  faEnvelope,
+  faFileAlt,
+  faGamepad,
+  faInfoCircle,
+  faPalette,
+  faPlug,
+  faProjectDiagram,
+  faSearch,
+  faUsers
+} from '@fortawesome/free-solid-svg-icons';
+
 import styles from '../../styles/admin.module.css';
 import MenuSelector from './MenuSelector';
 export default function Menu(properties) {
@@ -7,41 +21,68 @@ export default function Menu(properties) {
         name="Status"
         selected={properties.selected}
         route="status"
+        icon={faInfoCircle}
       />
       <MenuSelector
         name="Graphs"
         selected={properties.selected}
         route="graphs"
+        icon={faProjectDiagram}
       />
-      <MenuSelector name="Log" selected={properties.selected} route="log" />
-      <MenuSelector name="Mail" selected={properties.selected} route="mail" />
+      <MenuSelector
+        name="Log"
+        selected={properties.selected}
+        route="log"
+        icon={faFileAlt}
+      />
+      <MenuSelector
+        name="Mail"
+        selected={properties.selected}
+        route="mail"
+        icon={faEnvelope}
+      />
       <MenuSelector
         name="Plugins"
         selected={properties.selected}
         route="plugins"
+        icon={faPlug}
       />
       <MenuSelector
         name="Registries"
         selected={properties.selected}
         route="registries"
+        icon={faPagelines}
       />
       <MenuSelector
         name="Remotes"
         selected={properties.selected}
         route="remotes"
+        icon={faGamepad}
       />
       <MenuSelector
         name="SBOLExplorer"
         selected={properties.selected}
         route="explorer"
+        icon={faSearch}
       />
       <MenuSelector
         name="SPARQL"
         selected={properties.selected}
         route="sparql"
+        icon={faDatabase}
       />
-      <MenuSelector name="Theme" selected={properties.selected} route="theme" />
-      <MenuSelector name="Users" selected={properties.selected} route="users" />
+      <MenuSelector
+        name="Theme"
+        selected={properties.selected}
+        route="theme"
+        icon={faPalette}
+      />
+      <MenuSelector
+        name="Users"
+        selected={properties.selected}
+        route="users"
+        icon={faUsers}
+      />
     </div>
   );
 }
--- frontend/components/AdminComponents/MenuSelector.js
+++ frontend/components/AdminComponents/MenuSelector.js
@@ -1,18 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 import styles from '../../styles/admin.module.css';
 
 export default function MenuSelector(properties) {
-  const [style, setStyle] = useState('');
+  const [style, setStyle] = useState(styles.menuselectoractive);
   useEffect(() => {
     if (properties.selected && properties.selected === properties.route)
       setStyle(styles.menuselectorselected);
-    else setStyle('');
+    else setStyle(styles.menuselectoractive);
   }, [properties.selected]);
   return (
     <Link href={`/admin/${properties.route}`}>
-      <a className={`${styles.menuselector} ${style}`}>{properties.name}</a>
+      <a className={`${styles.menuselector} ${style}`}>
+        <div className={styles.menuselectoricon}>
+          <FontAwesomeIcon icon={properties.icon} size="1x" />
+        </div>
+        {properties.name}
+      </a>
     </Link>
   );
 }
