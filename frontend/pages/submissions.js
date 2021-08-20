b917f960ca5a29c75f5f2777b9ee9ec30b7cf603
benjhatch <benjamin.hat4@gmail.com>
2021-08-19 18:56:49 -0600

    created new submissions page

--- frontend/pages/submissions.js
+++ frontend/pages/submissions.js
@@ -1,55 +1,84 @@
 import axios from 'axios';
-import Loader from 'react-loader-spinner';
 import { useSelector } from 'react-redux';
 import useSWR from 'swr';
 
 import Basket from '../components/Basket';
-import ResultTable from '../components/SearchComponents/StandardSearch/ResultTable/ResultTable';
+import Table from '../components/ReusableComponents/Table/Table';
 import TopLevel from '../components/TopLevel';
 import styles from '../styles/submissions.module.css';
 
-export default function Submissions() {
-  const token = useSelector(state => state.user.token);
-  const { submissions, isMySubmissionsLoading, isMySubmissionsError } =
-    useMySubmissions(token);
-  const { shared, isSharedLoading, isSharedError } =
-    useSharedSubmissions(token);
+const searchable = ['name', 'displayId', 'type', 'description'];
+const headers = ['Name', 'Display ID', 'Description', 'Type'];
 
-  var content = null;
-  if (isMySubmissionsLoading || isSharedLoading) {
-    content = (
-      <div className={styles.loadercontainer}>
-        <div className={styles.loaderanimation}>
-          <Loader color="#D25627" type="ThreeDots" />
-        </div>
-      </div>
-    );
-  } else if (isMySubmissionsError || isSharedError) {
-    content = (
-      <div className={styles.error}>
-        Errors were encountered while fetching your submissions
-      </div>
-    );
-  } else {
-    content = (
-      <ResultTable
-        count={submissions.length}
-        data={[...submissions, ...shared]}
-        overrideType="Collection"
-        submissionsPage={true}
-      />
-    );
-  }
+function Submissions() {
+  const token = useSelector(state => state.user.token);
+  const { submissions, isMySubmissionsLoading } = useMySubmissions(token);
+  const { shared, isSharedLoading } = useSharedSubmissions(token);
   return (
-    <TopLevel>
-      <div className={styles.container}>
-        <div className={styles.pageheader}></div>
-        <Basket />
-        {content}
+    <div className={styles.container}>
+      <div className={styles.pageheader}></div>
+      <Basket />
+      <div className={styles.content}>
+        <SubmissionsTable
+          token={token}
+          title="Private"
+          loading={isMySubmissionsLoading}
+          data={submissions ? submissions : undefined}
+        />
+        <SubmissionsTable
+          token={token}
+          title="Shared With Me"
+          loading={isSharedLoading}
+          data={shared ? shared : undefined}
+        />
       </div>
-    </TopLevel>
+    </div>
   );
 }
+
+function SubmissionsTable(properties) {
+  return (
+    <div className={styles.submissiontable}>
+      <Table
+        data={properties.data}
+        loading={properties.loading}
+        title={properties.title}
+        searchable={searchable}
+        headers={headers}
+        sortOptions={options}
+        defaultSortOption={options[0]}
+        sortMethods={sortMethods}
+        hideFooter={true}
+        dataRowDisplay={submission => (
+          <tr key={submission.displayId}>
+            <td>
+              <code>{submission.name}</code>
+            </td>
+            <td>{submission.displayId}</td>
+            <td>{submission.description}</td>
+            <td>{submission.type}</td>
+          </tr>
+        )}
+      />
+    </div>
+  );
+}
+
+const options = [
+  { value: 'name', label: 'Name' },
+  { value: 'displayId', label: 'Display ID' },
+  { value: 'type', label: 'Type' }
+];
+
+const sortString = (plugin1, plugin2) => {
+  return (plugin1.name > plugin2.name && 1) || -1;
+};
+
+const sortMethods = {
+  name: sortString,
+  displayId: sortString,
+  type: sortString
+};
 
 const useMySubmissions = token => {
   const { data, error } = useSWR(
@@ -76,6 +105,14 @@ const useSharedSubmissions = token => {
     isSharedError: error
   };
 };
+
+export default function SubmissionsWrapped() {
+  return (
+    <TopLevel>
+      <Submissions />
+    </TopLevel>
+  );
+}
 
 const fetcher = (url, token) =>
   axios
