7157bb1a07520dbc355f70ce9056a1664e340bf0
benjhatch <benjamin.hat4@gmail.com>
2021-08-19 21:01:32 -0600

    made submissions page with all submissions in one table

---
+++ frontend/components/ReusableComponents/TableButton.js
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+
+import styles from '../../styles/defaulttable.module.css';
+
+export default function TableButton(properties) {
+  const [buttonClass, setButtonClass] = useState(styles.disabled);
+
+  useEffect(() => {
+    setButtonClass(properties.enabled ? styles.enabled : styles.disabled);
+  }, [properties.enabled]);
+
+  return (
+    <div
+      className={`${styles.tablebutton} ${buttonClass} ${styles.rightspace}`}
+    >
+      <span className={styles.buttonicon}>
+        <FontAwesomeIcon icon={properties.icon} color="#00000" size="1x" />
+      </span>
+      {properties.title}
+    </div>
+  );
+}
--- frontend/pages/submissions.js
+++ frontend/pages/submissions.js
@@ -1,73 +1,235 @@
+import {
+  faDownload,
+  faGlobeAmericas,
+  faPlus,
+  faShareAlt,
+  faTrashAlt,
+  faUserLock
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import useSWR from 'swr';
 
 import Basket from '../components/Basket';
 import Table from '../components/ReusableComponents/Table/Table';
+import TableButton from '../components/ReusableComponents/TableButton';
 import TopLevel from '../components/TopLevel';
 import styles from '../styles/submissions.module.css';
 
-const searchable = ['name', 'displayId', 'type', 'description'];
-const headers = ['Name', 'Display ID', 'Description', 'Type'];
+const searchable = ['name', 'displayId', 'type', 'description', 'privacy'];
 
 function Submissions() {
   const token = useSelector(state => state.user.token);
   const { submissions, isMySubmissionsLoading } = useMySubmissions(token);
   const { shared, isSharedLoading } = useSharedSubmissions(token);
+  const [processedData, setProcessedData] = useState([]);
+  const [selected, setSelected] = useState(new Map());
+  const [selectAll, setSelectAll] = useState(false);
+  const [buttonEnabled, setButtonEnabled] = useState(false);
+
+  useEffect(() => {
+    if (processedData) {
+      let checklist = new Map();
+      for (const submission of processedData)
+        checklist.set(submission.displayId, false);
+      setSelected(checklist);
+    }
+  }, [processedData]);
+
+  useEffect(() => {
+    if (selected && selected.size > 0) {
+      let allSelected = true;
+      let oneSelected = false;
+      for (const checked of selected.values()) {
+        if (!checked) {
+          allSelected = false;
+        } else {
+          oneSelected = true;
+        }
+      }
+      setSelectAll(allSelected);
+      setButtonEnabled(oneSelected);
+    }
+  }, [selected, processedData]);
+
+  useEffect(() => {
+    if (submissions && shared) {
+      for (const element of shared) {
+        element.privacy = 'shared';
+      }
+      setProcessedData(processSubmissions([...submissions, ...shared]));
+    }
+  }, [shared, submissions]);
+
   return (
     <div className={styles.container}>
-      <div className={styles.pageheader}></div>
       <Basket />
       <div className={styles.content}>
-        <SubmissionsTable
-          token={token}
-          title="Private"
-          loading={isMySubmissionsLoading}
-          data={submissions ? submissions : undefined}
-        />
-        <SubmissionsTable
-          token={token}
-          title="Shared With Me"
-          loading={isSharedLoading}
-          data={shared ? shared : undefined}
+        <div className={styles.buttonscontainer}>
+          <TableButton
+            title="Add to Basket"
+            enabled={buttonEnabled}
+            icon={faPlus}
+          />
+          <TableButton
+            title="Download"
+            enabled={buttonEnabled}
+            icon={faDownload}
+          />
+          <TableButton
+            title="Publish"
+            enabled={buttonEnabled}
+            icon={faGlobeAmericas}
+          />
+          <TableButton
+            title="Remove"
+            enabled={buttonEnabled}
+            icon={faTrashAlt}
+          />
+        </div>
+        <Table
+          data={processedData}
+          loading={isMySubmissionsLoading || isSharedLoading}
+          title="My Submissions"
+          searchable={searchable}
+          headers={[
+            <input
+              key={0}
+              checked={selectAll}
+              onChange={event => {
+                let checklist = new Map();
+                for (const submission of processedData)
+                  checklist.set(submission.displayId, event.target.checked);
+                setSelected(checklist);
+                setSelectAll(event.target.checked);
+              }}
+              type="checkbox"
+            />,
+            'Name',
+            'Display ID',
+            'Description',
+            'Type',
+            'Privacy'
+          ]}
+          sortOptions={options}
+          defaultSortOption={options[0]}
+          sortMethods={sortMethods}
+          numberShownLabel=" "
+          updateRowsWhen={selected}
+          dataRowDisplay={submission => (
+            <SubmissionDisplay
+              key={submission.displayId}
+              submission={submission}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          )}
         />
       </div>
     </div>
   );
 }
 
-function SubmissionsTable(properties) {
+function SubmissionDisplay(properties) {
+  const router = useRouter();
+
+  const [privacyDisplay, setPrivacyDisplay] = useState();
+
+  useEffect(() => {
+    if (properties.submission.privacy === 'public')
+      setPrivacyDisplay(<FontAwesomeIcon icon={faGlobeAmericas} size="1x" />);
+    else if (properties.submission.privacy === 'private')
+      setPrivacyDisplay(
+        <FontAwesomeIcon icon={faUserLock} color="#ff0000" size="1x" />
+      );
+    else setPrivacyDisplay(<FontAwesomeIcon icon={faShareAlt} size="1x" />);
+  }, [properties.submission.privacy]);
+
   return (
-    <div className={styles.submissiontable}>
-      <Table
-        data={properties.data}
-        loading={properties.loading}
-        title={properties.title}
-        searchable={searchable}
-        headers={headers}
-        sortOptions={options}
-        defaultSortOption={options[0]}
-        sortMethods={sortMethods}
-        hideFooter={true}
-        dataRowDisplay={submission => (
-          <tr key={submission.displayId}>
-            <td>
-              <code>{submission.name}</code>
-            </td>
-            <td>{submission.displayId}</td>
-            <td>{submission.description}</td>
-            <td>{submission.type}</td>
-          </tr>
-        )}
-      />
-    </div>
+    <tr
+      key={properties.submission.displayId}
+      className={styles.submission}
+      onClick={() => {
+        router.push(
+          properties.submission.uri.replace(
+            'https://synbiohub.org',
+            process.env.backendUrl
+          )
+        );
+      }}
+    >
+      <td>
+        <input
+          checked={properties.selected.get(properties.submission.displayId)}
+          onChange={event => {
+            properties.setSelected(
+              new Map(
+                properties.selected.set(
+                  properties.submission.displayId,
+                  event.target.checked
+                )
+              )
+            );
+          }}
+          onClick={event => {
+            event.stopPropagation();
+          }}
+          type="checkbox"
+        />
+      </td>
+      <td>
+        <code>{properties.submission.name}</code>
+      </td>
+      <td>{properties.submission.displayId}</td>
+      <td>{properties.submission.description}</td>
+      <td>{properties.submission.type}</td>
+      <td>{privacyDisplay}</td>
+    </tr>
   );
 }
+
+const processSubmissions = submissions => {
+  for (const submission of submissions) {
+    const potentialType = submission.type.toLowerCase();
+
+    // Identify what type of object the search result is from type url
+    if (potentialType.includes('component')) {
+      submission.type = 'Component';
+    }
+    if (potentialType.includes('sequence')) {
+      submission.type = 'Sequence';
+    }
+    if (potentialType.includes('module')) {
+      submission.type = 'Module';
+    }
+    if (potentialType.includes('collection')) {
+      submission.type = 'Collection';
+    }
+
+    if (submission.privacy !== 'shared') {
+      submission.privacy = 'public';
+      if (!submission.uri.includes('/public/')) submission.privacy = 'private';
+    }
+  }
+
+  return submissions;
+
+  /*
+  let privacy = <FontAwesomeIcon icon={faGlobeAmericas} size="1x" />;
+  if (!properties.uri.includes('/public/'))
+    privacy = <FontAwesomeIcon icon={faUserLock} color="#ff0000" size="1x" />;
+
+  */
+};
 
 const options = [
   { value: 'name', label: 'Name' },
   { value: 'displayId', label: 'Display ID' },
-  { value: 'type', label: 'Type' }
+  { value: 'type', label: 'Type' },
+  { value: 'privacy', label: 'Privacy' }
 ];
 
 const sortString = (plugin1, plugin2) => {
@@ -77,7 +239,8 @@ const sortString = (plugin1, plugin2) => {
 const sortMethods = {
   name: sortString,
   displayId: sortString,
-  type: sortString
+  type: sortString,
+  privacy: sortString
 };
 
 const useMySubmissions = token => {
