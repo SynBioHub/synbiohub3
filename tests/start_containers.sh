24dba3dd8f94e73e85596ec8e376cf95622aa27d
anka6130 <59932615+anka6130@users.noreply.github.com>
2023-02-15 14:05:08 -0700

    changed filepath

--- tests/start_containers.sh
+++ tests/start_containers.sh
@@ -7,15 +7,15 @@ message "Cleaning old test containers if they exist"
 bash ./testcleanup.sh
 
 
-message "pulling anjalakaturi/synbiohub-docker"
-if cd synbiohub-docker; then
-    git checkout snapshot;
-    git pull;
-    cd ..;
-else
-    # clone the synbiohub docker compose file in order to run docker containers
-    git clone --single-branch --branch snapshot https://github.com/anjalakaturi/synbiohub-docker
-fi
+# message "pulling anjalakaturi/synbiohub-docker"
+# if cd synbiohub-docker; then
+#     git checkout snapshot;
+#     git pull;
+#     cd ..;
+# else
+#     # clone the synbiohub docker compose file in order to run docker containers
+#     git clone --single-branch --branch snapshot https://github.com/anjalakaturi/synbiohub-docker
+# fi
 
 
 bash ./start_containers_persist.sh
