ce9ced264ec52cba8dcb77aa16cc1b33a76d136b
benjhatch <benjamin.hat4@gmail.com>
2023-02-21 16:35:25 -0700

    added json pages for a multitude of SBOL2 types for rendering

--- frontend/components/Viewing/PageJSON/MasterJSON.js
+++ frontend/components/Viewing/PageJSON/MasterJSON.js
@@ -1,11 +1,19 @@
 import ComponentDefinition from './Types/ComponentDefinition.json';
 import Collection from './Types/Collection.json';
 import Sequence from './Types/Sequence.json';
+import Model from './Types/Model.json';
+import ModuleDefinition from './Types/ModuleDefinition.json';
+import Implementation from './Types/Implementation.json';
+import Range from './Types/Range.json';
 
 const TypeToJson = {
   'http://sbols.org/v2#ComponentDefinition': ComponentDefinition,
   'http://sbols.org/v2#Collection': Collection,
-  'http://sbols.org/v2#Sequence': Sequence
+  'http://sbols.org/v2#Sequence': Sequence,
+  'http://sbols.org/v2#Model': Model,
+  'http://sbols.org/v2#ModuleDefinition': ModuleDefinition,
+  'http://sbols.org/v2#Implementation': Implementation,
+  'http://sbols.org/v2#Range': Range
 };
 
 export default TypeToJson;
