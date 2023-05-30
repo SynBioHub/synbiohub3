8c1f02cb62b455bec22d5c15446b4b136ff777cc
anjalakaturi <59932615+anjalakaturi@users.noreply.github.com>
2023-05-30 17:04:40 -0600

    Update test_before_login.py

--- tests/test_before_login.py
+++ tests/test_before_login.py
@@ -8,17 +8,11 @@ class TestBeforeLogin(TestCase):
     def test_before_login(self):
         headers = {"Accept": "text/plain"}
 
-        # test_post_bad_login(self):
-        test_print("test_post_bad_login starting")
-        bad_login_info = {'email' : 'bademail',
-                          'password' : 'test'}
-        compare_post_request("/login", bad_login_info, test_name='bad_email', headers=headers)
-        test_print("test_post_bad_login completed")
-
-        test_print("test_post_login starting")
+        test_print("test_not_registered_email starting")
         not_registered_email = {'email' : 'test7@user.synbiohub',
                       'password' : 'test'}
         compare_post_request("/login", not_registered_email, test_name='not_registered_email', headers=headers)
+        test_print("test_not_registered_email completed")
 
         #test_no_username_login(self):
         test_print("test_no_username_login starting")
