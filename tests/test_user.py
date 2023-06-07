bde99b4c43ba884e142348da3fb5c8398204144c
anjalakaturi <59932615+anjalakaturi@users.noreply.github.com>
2023-06-07 11:58:05 -0600

    update with updated compare func

--- tests/test_user.py
+++ tests/test_user.py
@@ -55,7 +55,7 @@ class TestUser(TestCase):
                       'password' : 'test'}
         login_with(logininfo, 1)
         
-        compare_get_request_json("/profile", headers = headers, route_parameters = [], test_type = test_type, test_name="admin_get_profile", fields=["name", "username", "email", "affiliation", "graphUri"])
+        compare_get_request("/profile", headers = headers, route_parameters = [], test_type = test_type, comparison_type = "json", test_name="admin_get_profile", fields=["name", "username", "email", "affiliation", "graphUri"])
 
         #log back in as a regular user
         logininfo = {'email' : 'test1@user.synbiohub',
