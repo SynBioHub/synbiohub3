9fb970acee808f142fdc8c1bc9cc4ec25cd83f74
Anjala Katuri <anka6130@colorado.edu>
2023-06-21 21:46:50 -0500

    cleanup search endpoints & start submit

--- tests/test_search.py
+++ tests/test_search.py
@@ -11,15 +11,16 @@ class TestSearch(TestCase):
         headers = {"Accept": "text/plain"}
         test_type = "Search"
 
+        #Search Metadata 
+
         # test_searchQuery(self):
         test_print("test_search starting")
         # #compare_get_request("/search/:query?", route_parameters = ["I0462"])
         compare_get_request("/search/:query?", headers = headers, route_parameters = ["BBa_B00"], test_type = test_type, comparison_type="jsonlist", fields=["uri", "displayId", "version", "name", "description", "type"], key='uri')
         test_print("test_search completed")
 
-        # test_searchCount(self):
         test_print("test_searchCount starting")
-        # #compare_get_request("/searchCount/:query?", route_parameters = ["I0462"]) 
+        #compare_get_request("/searchCount/:query?", route_parameters = ["I0462"]) 
         compare_get_request("/searchCount/:query?", headers = headers, route_parameters = ["BBa_B00"], test_type = test_type)
         compare_get_request("/searchCount/:query?", headers = headers, test_name = "searchCount0", route_parameters = ["abcdefg"], test_type = test_type)
         test_print("test_searchCount completed")
@@ -28,15 +29,23 @@ class TestSearch(TestCase):
         compare_get_request("/rootCollections", headers = {"Accept":"text/plain"}, route_parameters = [], test_type = test_type)
         test_print("test_rootCollections completed")
 
-        # #test_sparql(self):
-        test_print("test_sparql starting")
-        compare_get_request("/sparql?query=:query", headers = {"Accept":"application/json"}, route_parameters = ["SELECT+%3Fsubject+%3Fpredicate+%3Fobject+WHERE+%7B+%3Fsubject+%3Fpredicate+%3Fobject+.+FILTER+%28str%28%3Fobject%29+%3D+%22BBa_B0034%22%29%7D"], test_type = test_type, comparison_type="json")
-        test_print("test_sparql completed")
+        #"/manage" tested in test_submit.py
+
+        #TODO: shared - Returns the meta data on objects that other users have shared with the user specified by the X-authorization token.
 
         test_print("test_subcollections_public starting")
         # need user - compare_get_request("/public/:collectionId/:displayId/:version/subCollections", route_parameters = ["testid1","testid1_collection", "1"], headers = {"Accept":"text/plain"}, test_type = test_type)
         compare_get_request("/public/:collectionId/:displayId/:version/subCollections", route_parameters = ["igem","categories", "1"], headers = {"Accept":"text/plain"}, test_name="subCollectionsCategories", test_type = test_type, comparison_type="jsonlist", fields=["uri", "displayId", "version", "name", "description"], key='uri')
         test_print("test_subcollections_public completed")
+
+        #TODO: when submit works on SBH3
+        # test_print("test_subcollections_private starting")
+        # compare_get_request("/user/:userId/:collectionId/:displayId/:version/subCollections", route_parameters = ["testuser","testid2","testid2_collection", "1"],headers = {"Accept":"text/plain"}, test_name="privateSubCollection")
+        # test_print("test_subcollections_private completed")
+
+        #"/twins" tested in test_twins.py
+
+        #TODO: /similar - SBOLExplorer needs to be on 
 
         test_print("test_uses starting")
         # # need user - compare_get_request("user/:userId/:collectionId/:displayId/:version/uses", route_parameters = ["testuser1","testid2", "BBa_B0015", "1"],headers = {"Accept": "text/html"}, test_type = test_type)
@@ -49,9 +58,17 @@ class TestSearch(TestCase):
         compare_get_request(":type/count", route_parameters = ["xxx"], test_name="count0",headers = {"Accept":"text/plain"}, test_type = test_type)
         test_print("test_count completed")
 
-        # test_print("test_subcollections_private starting")
-        # compare_get_request("/user/:userId/:collectionId/:displayId/:version/subCollections", route_parameters = ["testuser","testid2","testid2_collection", "1"],headers = {"Accept":"text/plain"}, test_name="privateSubCollection")
-        # test_print("test_subcollections_private completed")
+        test_print("test_sparql starting")
+        compare_get_request("/sparql?query=:query", headers = {"Accept":"application/json"}, route_parameters = ["SELECT+%3Fsubject+%3Fpredicate+%3Fobject+WHERE+%7B+%3Fsubject+%3Fpredicate+%3Fobject+.+FILTER+%28str%28%3Fobject%29+%3D+%22BBa_B0034%22%29%7D"], test_type = test_type, comparison_type="json")
+        test_print("test_sparql completed")
+
+        #TODO: sequence search, not done in SBH3
+
+        
+
+        
+
+        
         
 
 
--- tests/test_submit.py
+++ tests/test_submit.py
@@ -6,38 +6,31 @@ from test_functions import compare_get_request, compare_post_request, get_reques
 class TestSubmit(TestCase):
     def test_submit(self):
         test_type = "Submit"
-        test_print("test_main_page starting")
         headers = {'Accept':'text/plain'}
-        #compare_get_request("/", test_name = "after_admin_login", headers = headers)
-        get_request("", 1, headers = headers, route_parameters = [])
-
-        test_print("test_main_page completed")
 
         test_print("test_get_submit_submissions_empty starting")
-        #compare_get_request("submit")
-        #compare_get_request("manage")
 
         # working curl request
         #curl -X POST -H "Accept: text/plain" -H "X-authorization: e35054aa-04e3-425c-afd2-66cb95ff66e1" -F id=green -F version="3" -F name="test" -F description="testd" -F citations="none" -F overwrite_merge="0" -F file=@"./SBOLTestRunner/src/main/resources/SBOLTestSuite/SBOL2/BBa_I0462.xml" http://localhost:7777/submit"""
 
         test_print("test_get_submit_submissions_empty completed")
 
-#TODO: can uncomment when works in sbh3
-        # test_print("test_create_id_missing starting")
+        #TODO: can uncomment when works in sbh3
+        test_print("test_create_id_missing starting")
 
-        # data = {'version' : (None, '1'),
-        #         'name' : (None, 'testcollection'),
-        #         'description':(None, 'testdescription'),
-        #         'citations':(None, ''),
-        #         'overwrite_merge':(None, '0')}
+        data = {'version' : (None, '1'),
+                'name' : (None, 'testcollection'),
+                'description':(None, 'testdescription'),
+                'citations':(None, ''),
+                'overwrite_merge':(None, '0')}
 
-        # files = {'file':("./SBOLTestRunner/src/main/resources/SBOLTestSuite/SBOL2/BBa_I0462.xml",
-        #                                       open('./SBOLTestRunner/src/main/resources/SBOLTestSuite/SBOL2/BBa_I0462.xml', 'rb'))}
-        # with self.assertRaises(requests.exceptions.HTTPError):
-        #     #compare_post_request("submit", data, headers = {"Accept": "text/plain"}, files = files, test_name = "missing_id", test_type = test_type)
-        #         post_request("submit", 1, data, headers = headers, route_parameters = [], files = files)
+        files = {'file':("./SBOLTestRunner/src/main/resources/SBOLTestSuite/SBOL2/BBa_I0462.xml",
+                                              open('./SBOLTestRunner/src/main/resources/SBOLTestSuite/SBOL2/BBa_I0462.xml', 'rb'))}
+        with self.assertRaises(requests.exceptions.HTTPError):
+            #compare_post_request("submit", data, headers = {"Accept": "text/plain"}, files = files, test_name = "missing_id", test_type = test_type)
+                post_request("submit", 1, data, headers = headers, route_parameters = [], files = files)
 
-        # test_print("test_create_id_missing completed")
+        test_print("test_create_id_missing completed")
 
         test_print("test_create_and_delete_collections starting")
         # create the collection
