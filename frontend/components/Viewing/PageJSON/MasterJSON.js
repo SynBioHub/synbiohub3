930fb985b479e74589ecb254553803cbc0d970f9
George Guo <george.guo414@gmail.com>
2023-07-18 11:07:19 -0600

    finished Interaction json with a few minor bugfixes needed after meeting with Ben

--- frontend/components/Viewing/PageJSON/MasterJSON.js
+++ frontend/components/Viewing/PageJSON/MasterJSON.js
@@ -13,6 +13,7 @@ import MapsTo from './Types/MapsTo.json';
 import FunctionalComponent from './Types/FunctionalComponent.json';
 import CombinatorialDerivation from './Types/CombinatorialDerivation.json';
 import Module from './Types/Module.json';
+import Interaction from './Types/Interaction.json';
 
 
 const TypeToJson = {
@@ -30,7 +31,8 @@ const TypeToJson = {
   'http://sbols.org/v2#MapsTo': MapsTo,
   'http://sbols.org/v2#FunctionalComponent': FunctionalComponent,
   'http://sbols.org/v2#CombinatorialDerivation': CombinatorialDerivation,
-  'http://sbols.org/v2#Module': Module
+  'http://sbols.org/v2#Module': Module,
+  'http://sbols.org/v2#Interaction': Interaction
 
 };
 
