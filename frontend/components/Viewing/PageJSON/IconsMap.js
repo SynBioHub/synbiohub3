bb9778cf300a3c4ade1117ca6b76c285812edc7d
learner97 <dfang97@yahoo.com>
2023-09-26 17:25:52 -0600

    modified several icons for multiple pages

--- frontend/components/Viewing/PageJSON/IconsMap.js
+++ frontend/components/Viewing/PageJSON/IconsMap.js
@@ -2,14 +2,30 @@ import {
   faInfoCircle,
   faVials,
   faPuzzlePiece,
-  faDna
+  faDna,
+  faThLarge,
+  faToolbox,
+  faHandshake,
+  faMapMarkedAlt,
+  faRuler,
+  faPeopleCarry,
+  faUsers,
+  faPlug
 } from '@fortawesome/free-solid-svg-icons';
 
 const IconsMap = {
   faInfoCircle,
   faVials,
   faPuzzlePiece,
-  faDna
+  faDna,
+  faThLarge,
+  faToolbox,
+  faHandshake,
+  faMapMarkedAlt,
+  faRuler,
+  faPeopleCarry,
+  faUsers,
+  faPlug
 };
 
 export default IconsMap;
