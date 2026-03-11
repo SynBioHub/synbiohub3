33e441d55b6d2190062a0cb92a7929058ad36fa6
learner97 <dfang97@yahoo.com>
2026-03-11 12:14:45 -0600

    should be a fix for rendering of plugins failing with properties not defined error

--- frontend/components/Viewing/Sections/Plugin.js
+++ frontend/components/Viewing/Sections/Plugin.js
@@ -109,7 +109,7 @@ export default function Plugin(properties) {
     return (
       <Section title={properties.title} pluginID={properties.pluginID} >
         <div id={properties.plugin.name}>
-          {parse(`${content}`, options)}
+          {parse(`${content}`, getOptions(properties.plugin.name))}
         </div>
       </Section>
       
@@ -200,41 +200,42 @@ async function runPlugin(plugin, pluginData, pluginsUseLocalCompose, pluginLocal
   })
   
 }
+const acceptedTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'img', 'ul', 'ol', 'li', 'table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'div', 'span', 'br', 'hr', 'pre', 'code', 'blockquote', 'strong', 'em', 'i', 'b', 'u', 's', 'sub', 'sup', 'del', 'ins', 'mark', 'small', 'big', 'abbr', 'cite', 'dfn', 'kbd', 'q', 'samp', 'var', 'time', 'address', 'article', 'aside', 'footer', 'header', 'nav', 'section', 'main', 'figure', 'figcaption', 'details', 'summary', 'dialog', 'menu', 'menuitem', 'menuitem', 'meter', 'progress', 'output', 'canvas', 'audio', 'video', 'iframe', 'object', 'embed', 'param', 'source', 'track', 'map', 'area', 'form', 'label', 'input', 'button', 'select', 'datalist', 'optgroup', 'option', 'textarea', 'fieldset', 'legend', 'datalist', 'output', 'progress', 'meter', 'details', 'summary', 'command', 'menu', 'menuitem', 'menuitem', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', 'map', 'track', 'source', 'param', 'iframe', 'embed', 'object', 'canvas', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', 'map', 'track', 'source', 'param', 'iframe', 'embed', 'object', 'canvas', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', ];
 
-const acceptedTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'img', 'ul', 'ol', 'li', 'table', 'tr', 'td', 'th', 'thead', 'tbody', 'tfoot', 'caption', 'div', 'span', 'br', 'hr', 'pre', 'code', 'blockquote', 'strong', 'em', 'i', 'b', 'u', 's', 'sub', 'sup', 'del', 'ins', 'mark', 'small', 'big', 'abbr', 'cite', 'dfn', 'kbd', 'q', 'samp', 'var', 'time', 'address', 'article', 'aside', 'footer', 'header', 'nav', 'section', 'main', 'figure', 'figcaption', 'details', 'summary', 'dialog', 'menu', 'menuitem', 'menuitem', 'meter', 'progress', 'output', 'canvas', 'audio', 'video', 'iframe', 'object', 'embed', 'param', 'source', 'track', 'map', 'area', 'form', 'label', 'input', 'button', 'select', 'datalist', 'optgroup', 'option', 'textarea', 'fieldset', 'legend', 'datalist', 'output', 'progress', 'meter', 'details', 'summary', 'command', 'menu', 'menuitem', 'menuitem', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', 'map', 'track', 'source', 'param', 'iframe', 'embed', 'object', 'canvas', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', 'map', 'track', 'source', 'param', 'iframe', 'embed', 'object', 'canvas', 'script', 'noscript', 'style', 'link', 'meta', 'title', 'base', 'head', 'body', 'html', 'br', 'hr', 'wbr', 'img', 'area', ]
+const getOptions = pluginName => {
+  const options = {
+    replace: domNode => {
+      if (!domNode.attribs) {
+        return;
+      }
 
-const options = {
-      replace: domNode => {
-        if(!domNode.attribs) {
-          return;
+      if (domNode.name === '!doctype html') {
+        return <></>;
+      }
+
+      if (!acceptedTags.includes(domNode.name)) {
+        return <>{domToReact(domNode.children, options)}</>;
+      }
+
+      if (domNode.name === 'html' || domNode.name === 'head' || domNode.name === 'body') {
+        return <>{domToReact(domNode.children, options)}</>;
+      }
+
+      if (domNode.type === 'script' && pluginName) {
+        const script = document.createElement('script');
+        if (domNode.attribs.src) {
+          script.src = domNode.attribs.src;
         }
-
-        if(domNode.name === '!doctype html') {
-          return <></>;
+        script.type = 'text/javascript';
+        script.innerText = `${domToReact(domNode.children, options)}`;
+        const container = document.getElementById(pluginName);
+        if (container) {
+          container.appendChild(script);
         }
-
-        if(!acceptedTags.includes(domNode.name)) {
-          return (
-            <>{domToReact(domNode.children, options)}</>
-          );
-        }
-
-        if(domNode.name === 'html' || domNode.name === 'head' || domNode.name === 'body') {
-          return (
-            <>{domToReact(domNode.children, options)}</>
-          );
-        }
-
-        if(domNode.type === 'script') {
-          var script = document.createElement('script');
-          if(domNode.attribs.src) {
-            script.src = domNode.attribs.src;
-          }
-          script.type = 'text/javascript';
-          script.innerText = `${domToReact(domNode.children, options)}`;
-          document.getElementById(`${properties.plugin.name}`).appendChild(script);
-          return <></>;
-        }
-
+        return <></>;
       }
     }
+  };
+
+  return options;
+};
