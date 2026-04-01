1fe963a0fabb3f7c5642ebf2d534a85956188552
learner97 <dfang97@yahoo.com>
2026-03-31 18:54:11 -0600

    the change didn't help with uploading igem data so i reversed it

--- tests/add-igem-data/upload3.js
+++ tests/add-igem-data/upload3.js
@@ -1,9 +1,8 @@
 fs = require('fs');
+request = require('request');
 path = require('path');
-var postGraphCrud = require('./common-upload').postGraphCrud;
-
 function walk(currentDirPath, callback) {
-    fs.readdirSync(currentDirPath).forEach(function (name) {
+    fs.readdirSync(currentDirPath).forEach(function(name) {
         var filePath = path.join(currentDirPath, name);
         var stat = fs.statSync(filePath);
         if (stat.isFile()) {
@@ -16,54 +15,65 @@ function walk(currentDirPath, callback) {
 
 var files = [];
 
-walk('./out', function (filename) {
+walk('./out', function(filename) {
+
+    //console.log(filename)
     files.push(filename);
 });
 
+//console.log(JSON.stringify(files, null, 2))
+
+//var i = parseInt(fs.readFileSync('last') + '');
 var i = 0;
-var len = files.length;
+var len = files.length
 
 function next() {
-    if (i == len) {
+
+    if(i == len){
         return;
     }
     var filename = files[i];
     i++;
 
-    console.log('reading file ' + filename);
+console.log('reading file ' + filename)
 
-    postGraphCrud(
-        {
-            method: 'POST',
-            url: 'http://localhost:8891/sparql-graph-crud-auth/?graph-uri=https://synbiohub.org/public',
-            auth: {
-                user: 'dba',
-                pass: 'dba',
-                sendImmediately: false
-            },
-            headers: {
-                'Content-Type': 'application/rdf+xml'
-            },
-            body: fs.readFileSync(filename) + '',
-            label: 'virtuoso3',
-            filename: filename
+    request({
+        method: 'POST',
+        //url: 'http://biocad.ncl.ac.uk:3030/igem/upload',
+        url: 'http://localhost:8891/sparql-graph-crud-auth/?graph-uri=https://synbiohub.org/public',
+        //url: 'http://synbiohub.org:8080/openrdf-sesame/repositories/synbiohub/statements',
+        auth:  {
+            user: 'dba',
+            pass: 'dba',
+//            pass: 'nyr-Dbt-roh-QW3',
+            sendImmediately: false
         },
-        function (err, response, body) {
-            var sc = response && response.statusCode !== undefined ? response.statusCode : 'no-response';
-            console.log(
-                'submitted ' + filename + ' to virtuoso3 with err ' + err + ' and response ' + sc
-            );
-            console.log(body);
+        /*qs: {
+            'context': 'null'
+        },*/
+        headers: {
+            //'Content-Type': 'multipart/form-data'
+            'Content-Type': 'application/rdf+xml'
+        },
+        //formData: {
+            //file: fs.createReadStream(filename)
+        //}
+        body: fs.readFileSync(filename) + ''
+    }, function(err, response, body) {
 
-            if (err) {
-                console.log('error! ' + err);
-            } else {
-                fs.writeFileSync('last', i + '');
-            }
+        console.log('submitted ' + filename + ' to virtuoso3 with err ' + err + ' and response ' + response.statusCode);
+        console.log(body)
 
-            next();
+        if(err) {
+            console.log('error! ' + err);
+        } else {
+            fs.writeFileSync('last', i + '');
         }
-    );
+
+        next();
+
+        //callback(null, body);
+    });
 }
 
 next();
