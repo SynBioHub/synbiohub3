d5185587f59fc817ba7acb5d91e05bb6f20eda78
learner97 <dfang97@yahoo.com>
2026-08-27 14:04:04 -0600

    edited the tests so that it actually edits a private part

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -1,57 +1,48 @@
-import requests
 from unittest import TestCase
-from test_arguments import test_print
-from test_functions import compare_get_request, compare_post_request
+from test_arguments import test_print, SETUP_URI_PREFIX
+from test_functions import compare_post_request, login_with
+
+# Same private collection as test_collection.py / test_attachment.py (owned by setup user testuser).
+PRIVATE_TOP_LEVEL_URI = f"{SETUP_URI_PREFIX}user/testuser/testid2/testid2_collection/1"
+
 
 class TestEdit(TestCase):
 
     def test_edit(self):
+        login_with({'email': 'test@user.synbiohub', 'password': 'test'}, 1)
+
         # test_update_mutableDescription
         test_print("test_update_mutableDescription starting")
-        data={
-            'uri': 'http://localhost:7777/public/testid1/testid1_collection/1',
-            'value' : 'testUpdateMutableDescription'
+        data = {
+            'uri': PRIVATE_TOP_LEVEL_URI,
+            'value': 'testUpdateMutableDescription',
         }
-        compare_post_request("updateMutableDescription", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableDescription")
+        compare_post_request("updateMutableDescription", data, headers={"Accept": "text/plain"}, test_name="test_update_mutableDescription")
         test_print("test_update_mutableDescription completed")
 
         # test_update_mutableNotes
         test_print("test_update_mutableNotes starting")
-        data={
-            'uri': 'http://localhost:7777/public/testid1/testid1_collection/1',
-            'value' : 'testUpdateMutableNotes'
+        data = {
+            'uri': PRIVATE_TOP_LEVEL_URI,
+            'value': 'testUpdateMutableNotes',
         }
-        compare_post_request("updateMutableNotes", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableNotes")
+        compare_post_request("updateMutableNotes", data, headers={"Accept": "text/plain"}, test_name="test_update_mutableNotes")
         test_print("test_update_mutableNotes completed")
 
         # test_update_mutableSource
         test_print("test_update_mutableSource starting")
-        data={
-            'uri': 'http://localhost:7777/public/testid1/testid1_collection/1',
-            'value' : 'testUpdateMutableSource'
+        data = {
+            'uri': PRIVATE_TOP_LEVEL_URI,
+            'value': 'testUpdateMutableSource',
         }
-        compare_post_request("updateMutableSource", data, headers = {"Accept": "text/plain"},test_name = "test_update_mutableSource")
+        compare_post_request("updateMutableSource", data, headers={"Accept": "text/plain"}, test_name="test_update_mutableSource")
         test_print("test_update_mutableSource completed")
 
         # test_edit_citations
         test_print("test_edit_citations starting")
-        data={
-            'uri': 'http://localhost:7777/public/testid1/testid1_collection/1',
-            'value' : '1234'
+        data = {
+            'uri': PRIVATE_TOP_LEVEL_URI,
+            'value': '1234',
         }
-        compare_post_request("updateCitations", data, headers = {"Accept": "text/plain"},test_name = "test_edit_mutable_citations")
+        compare_post_request("updateCitations", data, headers={"Accept": "text/plain"}, test_name="test_edit_mutable_citations")
         test_print("test_edit_citations completed")
-
-#    def test_add_field(self):
-#        data={
-#            'object' : 'testAddField'
-#        }
-#        compare_post_request("http://localhost:7777/public/testid0/testid0_collection/1/add/title", data, headers = {"Accept": "text/plain"},test_name = "test_add_field")
-
-#    def test_edit_field(self):
-#        data={
-#            'previous' : 'testAddField',
-#            'object' : 'testEditField'
-#        }
-#        compare_post_request("http://localhost:7777/public/testid0/testid0_collection/1/add/title", data, headers = {"Accept": "text/plain"},test_name = "test_add_field")
-
